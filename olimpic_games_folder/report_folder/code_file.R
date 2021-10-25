1ebce75d3ab2d16142b007a39a177b1b8c0fd80a
Tatiana Kikalova <t_kikalova@bk.ru>
2021-10-25 21:27:46 +0200

    Data file 08 fixed

--- olimpic_games_folder/report_folder/code_file.R
+++ olimpic_games_folder/report_folder/code_file.R
@@ -1,21 +1,19 @@
+#  1. Надо объединить наблюдения в единую таблицу:
 comb_files <- function(ext) {
-  file_list <- list.files(path = ".\\data_folder\\")
-  if (length(file_list) > 0) {
-    first_file <- tryCatch(read.csv(file = paste0(".\\data_folder\\", file_list[1])))
-    print(file_list[1])
-    result_file <- first_file
-    for (file_name in file_list) {
-      if (which(file_name == file_list) != 1) {
-        input_file <- tryCatch(read.csv(file = paste0(".\\data_folder\\", file_name)))
-        print(file_name)
-        result_file <- rbind(result_file, input_file)
-      }
+  data_path <- ".\\data_folder\\"
+  file_list <- list.files(path = data_path)
+  first_file <- read.csv(file = paste0(data_path, file_list[1]))
+  result_file <- first_file 
+  
+  for (file_name in file_list) {
+    if (which(file_name == file_list) != 1) {
+      input_file <- read.csv(file = paste0(data_path, file_name))
+      result_file <- rbind(result_file, input_file) 
     }
-    return(result_file)
   }
-}
-
-
+  return(result_file)
+} 
 
 out_dataframe <- comb_files("csv")
 
+# 2. Исправьте некорректные данные:
